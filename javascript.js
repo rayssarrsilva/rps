73a6c6c8ca909ce0dc4e786bd97ea79a23a8452b
rayssa <116031703+rayssarrsilva@users.noreply.github.com>
2026-04-20 14:52:12 -0300

    Create the getComputerChoice function in js
    the function return if the computer choosed "rock", "paper" or "scissors"

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+function getComputerChoice() {
+    value = Math.floor(Math.random() * 3);
+    
+    if (value === 0) {
+        value = "Rock";
+    } else if (value === 1) {
+        value = "Paper";
+    } else {
+        value = "Scissors";
+    }
+    
+    return value;
+}
+
+console.log(getComputerChoice());
